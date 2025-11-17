b8110a120d3a6dee55badc2da6731fd0b968e6f1
Mordziarz <139069437+Mordziarz@users.noreply.github.com>
2025-11-17 13:42:43 +0100

    Update blast_trichoderma.R

--- R/blast_trichoderma.R
+++ R/blast_trichoderma.R
@@ -128,7 +128,7 @@ trichoderma_blast <- function(query_sequence = "",
     stop("No valid sequences found in query file")
   }
 
-  blast_result <- predict(blast_db, query_sequences, BLAST_args = "-task blastn -qcov_hsp_perc 100 -max_hsps 1")
+  blast_result <- predict(blast_db, query_sequences, BLAST_args = "-task blastn -qcov_hsp_perc 100")
 
   return(blast_result)
 }
